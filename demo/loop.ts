784e765e0c85c886953a33f7fc636e2158863ac8
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2026-06-20 03:32:18 +0530

    Add loop functionality; implement agent execution for listing directory contents

--- demo/loop.ts
+++ demo/loop.ts
@@ -1,3 +1,21 @@
 import { runAgent } from "../src/agent";
 import { ToolRegistry } from "../src/registry";
 import { listFiles } from "../src/tools/files";
+import { trace } from "./trace";
+
+const tools = new ToolRegistry().register(listFiles);
+
+const answer = await runAgent(
+    {
+        name: "explorer",
+        systemPrompt:
+            "You are code project structure reporter",
+        tools,
+        onEvent: (e) => {
+            trace(e);
+        },
+    },
+    "list the contents of node_modules/"
+);
+
+console.log("=== RESULT ===\n" + answer);
